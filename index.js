a9d289d757885c4c28a776b2e6ba3d84869f55e5
sallykhazaal <sallykhazaal@yahoo.com>
2023-01-22 19:22:07 -0500

    commit tree 0b63d324dfc6e7dd057c77c10f1b0feaaa7db747

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Sally is the champion";
+document.body.append(newHeader);
